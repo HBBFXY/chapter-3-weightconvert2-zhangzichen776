40d0cb9c1420de455f555e384d51a91ca67df129
zhangzichen776 <2589134403@qq.com>
2025-09-18 20:22:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# 在这个文件里编写代码
+earth_weight = float(input("请输入地球重量(kg)："))
+print("年份\t地球重量(kg)\t月球重量(kg)")
+for year in range(1, 11):
+    earth_weight+=0.5
+    moon_weight=earth_weight*0.165
+    print(f"{year}\t{earth_weight:.2f}\t\t{moon_weight:.2f}")
